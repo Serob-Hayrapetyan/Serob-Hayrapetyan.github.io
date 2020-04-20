6898035f6225c5efd19690d7b1bb8fc971c7e120
Serob-Hayrapetyan <hayrapetyan.serob@gmail.com>
2020-04-20 22:10:35 +0400

    Change for Anjelika

---
+++ assets/js/change.js
@@ -0,0 +1,5 @@
+function Change()
+{
+    var x = document.getElementById("myDIV");
+    x.innerHTML = '<img src="../images//main_images//layer1/layer1_move.jpg" width="100%">'
+}
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(session({
 }));
 
 
-
+//This method ....
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname))
